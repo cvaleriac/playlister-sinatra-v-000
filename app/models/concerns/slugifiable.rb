8a15b8001fb41b0a4af7e26bb1155330a5b8266d
valeria campilongo <cvaleriac@hotmail.it>
2019-01-15 16:44:54 +0000

    Automatically backed up by Learn

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,10 +1,11 @@
-module SlugifiableInstanceMethods
+module Slugifiable
+  module InstanceMethods
   def slug
     self.name.gsub(" ", "-").downcase
   end
 end
 
-module SlugifiableClassMethods
+module ClassMethods
   def find_by_slug(slug)
     self.all.find{ |instance| instance.slug == slug }
   end
